46c24d932c602f72851f44e925a46b3e8ab23eb7
AlekGDEV <AlexandreGDEV@outlook.com>
2022-10-10 14:00:02 -0300

    adicionei o react-router-dom, tipografia, e separei as paginas e componentes

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,32 +1,24 @@
-import './App.css';
+import Nav from "./components/Nav/index.js";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./Pages/Home"
+import Produtos from "./Pages/Produtos"
+import Categorias from "./Pages/Categorias"
+import MeusPedidos from "./Pages/MeusPedidos"
 
 export default function App() {
   return (
-    <div className="App">
-      <header>
-        <nav>
-
-        </nav>
-      </header>
-
-      <main>
-        <article>
-
-        </article>
-        <section>
-
-        </section>
-
-        <article>
-
-        </article>
-      </main>
-
-      <footer>
-
-      </footer>
+    <div>
+      <BrowserRouter>
+        <Nav/>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="produtos" element={<Produtos/>}></Route>
+            <Route path="categorias" element={<Categorias/>}></Route>
+            <Route path="meusPedidos" element={<MeusPedidos/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 
