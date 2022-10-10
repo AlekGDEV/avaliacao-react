490ef67930223e42bd993599e5a96bc066d08b36
ItaloLevi0 <italoleviti@gmail.com>
2022-10-10 18:58:29 -0300

    Estrutura montada, apenas o estilo do input radio faltando

--- e-commerce/src/aba-produtos/navbar.js
+++ e-commerce/src/aba-produtos/navbar.js
@@ -1,35 +1,84 @@
+import "bootstrap/dist/css/bootstrap.css"
 import "./style.css"
 
 export default function Navbar () 
 {
     return ( 
-        <nav>
+        
+        <nav >
             <span className="textFilter">Filtrar por</span>
-            <hr />
+            <hr/>
             <div className="itens">
-                <div className="nav-item">Marka</div>
-                <div className="nav-item"><input className="checkbox" type="checkbox" /> Adiddas</div>
-                <div className="nav-item"><input type="checkbox" /> Calenciaga</div>
-                <div className="nav-item"><input type="checkbox" /> K-Swiss</div>
-                <div className="nav-item"><input type="checkbox" /> Nike</div>
-                <div className="nav-item"><input type="checkbox" /> Puma</div>
-    
-                <div className="nav-item">Categoria</div>
-                <div className="nav-item"><input type="checkbox" /> Esporte e Lazer</div>
-                <div className="nav-item"><input type="checkbox" /> Casual</div>
-                <div className="nav-item"><input type="checkbox" /> Utilitário</div>
-                <div className="nav-item"><input type="checkbox" /> Corrida</div>
-    
-                <div>Gênero</div>
-                <div><input type="checkbox" /> Masculino</div>
-                <div><input type="checkbox" /> Feminino</div>
-                <div><input type="checkbox" /> Unisex</div>
+                <div className="nav-categoria mb-1 mt-2">Marca</div>
+                <div className="checkbox">
+                    <input id="check1" className="" type="checkbox"/>
+                    <label htmlFor="check1">Addidas</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check2" className="" type="checkbox"/>
+                    <label htmlFor="check2">Calenciaga</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check3" className="" type="checkbox"/>
+                    <label htmlFor="check3">K-Swiss</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check4" className="" type="checkbox"/>
+                    <label htmlFor="check4">Nike</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check5" className="" type="checkbox"/>
+                    <label htmlFor="check5">Puma</label>
+                </div>
 
-                <div>Estado</div>
-                <div><input type="checkbox" /> Novo</div>
-                <div><input type="checkbox" /> Usado</div>
+                <div className="nav-categoria mb-1 mt-2">Categoria</div>
+                <div className="checkbox">
+                    <input id="check6" className="" type="checkbox"/>
+                    <label htmlFor="check6">Esporte e lazer</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check7" className="" type="checkbox"/>
+                    <label htmlFor="check7">Casual</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check8" className="" type="checkbox"/>
+                    <label htmlFor="check8">Utilitário</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check9" className="" type="checkbox"/>
+                    <label htmlFor="check9">Corrida</label>
+                </div>
+
+                <div className="nav-categoria mb-1 mt-2">Gênero</div>
+                <div className="checkbox">
+                    <input id="check10" className="" type="checkbox"/>
+                    <label htmlFor="check10">Masculino</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check11" className="" type="checkbox"/>
+                    <label htmlFor="check11">Feminino</label>
+                </div>
+                <div className="checkbox">
+                    <input id="check12" className="" type="checkbox"/>
+                    <label htmlFor="check12">Unisex</label>
+                </div>
+                <div className="nav-categoria mb-1 mt-2">Estado</div>
+                <div className="radio">
+                    <input id="radio1" name="estado" value="1" type="radio"/>
+                    <label htmlFor="radio1">Novo</label>
+                </div>
+                <div className="radio">
+                    <input id="radio2" name="estado" value="2" type="radio"/>
+                    <label htmlFor="radio2">Usado</label>
+                </div>
+                {/* 
+    
+
+                <div><input className="form-check-input" type="radio" /> Novo</div>
+                <div><input className="form-check-input" type="radio" /> Usado</div> */}
             </div>
         </nav>
+        
         
     
 
