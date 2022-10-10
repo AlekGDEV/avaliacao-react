68beaf12efd510bcc8b8a2d17f31d69d7acfdfd7
TalysonSoares <talysonsoares40@gmail.com>
2022-10-10 15:49:50 -0300

    trabalhando no botão

--- e-commerce/src/Pages/Home/index.js
+++ e-commerce/src/Pages/Home/index.js
@@ -9,10 +9,10 @@ export default function BannerF() {
             </div>
 
             <div className="ofertat">
-                <p>Oferta especial</p>
+                <p className="p">Oferta especial</p>
                 <h1>Air Jordan edição de colecionador</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati animi optio eligendi et nisi aut quasi vero, est nostrum? Quaerat unde tempora magni neque nam. Sit recusandae repudiandae similique.</p>
-                <button>Ver oferta</button>
+                <button className="veroferta">Ver oferta</button>
             </div>
 
         </div>
