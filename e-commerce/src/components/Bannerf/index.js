2b6c6c4d31dd75d12b9f7ba162b1dfd5f8448c83
TalysonSoares <talysonsoares40@gmail.com>
2022-10-14 08:47:33 -0300

    estilizando titulo e texto

--- e-commerce/src/components/Bannerf/index.js
+++ e-commerce/src/components/Bannerf/index.js
@@ -11,7 +11,7 @@ export default function BannerF() {
             <div className="ofertat">
                 <p className="p">Oferta especial</p>
                 <h1 className="Jordan">Air Jordan edição de colecionador</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati animi optio eligendi et nisi aut quasi vero, est nostrum? Quaerat unde tempora magni neque nam. Sit recusandae repudiandae similique.</p>
+                <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
                 <button className="veroferta">Ver oferta</button>
             </div>
         </div>
