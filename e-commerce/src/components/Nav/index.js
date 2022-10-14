164dc414cc8e13ee38fde3e3eddc2e5cfd0bc1c5
ErikaNunex <erikanunes0512@gmail.com>
2022-10-14 02:56:53 -0300

    coloquei a nav como container e dei um wrap para quebrar melhor a linha

--- e-commerce/src/components/Nav/index.js
+++ e-commerce/src/components/Nav/index.js
@@ -5,12 +5,13 @@ import { Link, useLocation } from "react-router-dom"
 import "./../../css/app.css"
 import logoSimble from "./img/Vector.png"
 import search from "./img/Search.png"
+import carrinho from "./img/carrinho.png"
 import React from "react";
 
 export default function Nav(){
     return(
         <nav className="container">
-            <div className="agrupar">
+            <div className="elementos">
                 <div className="logo">
                    <img src={logoSimble}></img>
                     <h1 className="textLogo">Digital Store</h1>
@@ -22,24 +23,21 @@ export default function Nav(){
                         <img src={search}></img>
                     </div>
         
-                    <a href="">Cadastre-se</a>
-                    <button >Entrar</button>
-                    <button >
-                    <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.749878 1.25L2.82988 1.61L3.79288 13.083C3.87088 14.018 4.65188 14.736 5.58988 14.   736H16.5019C17.3979 14.736 18.1579 14.078 18.2869 13.19L19.2359 6.632C19.3529 5.823 18.7259 5.099  17.9089 5.099H3.16388" stroke="#C92071" stroke-width="1.5" stroke-linecap="round"    stroke-linejoin="round"/>
-                    </svg>
+                    <a className="linkCadastrar" href="">Cadastre-se</a>
+                    <button className="entrar">Entrar</button>
+                    <button className="botaoCarrinho" >
+                        <img src={carrinho} ></img>
                     </button>
                 </div>
+            </div>
+                
 
-                <div className="divMenor">
+                <div className="divLinks">
                     <a href="">Home</a>
                     <a href="">Produtos</a>
                     <a href="">Categorias</a>
                     <a href="">MeusPedidos</a>
                 </div>
-                
-            </div>
-
 
         </nav>
     )
