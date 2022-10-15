ac7402868cdc5a53e1ad7ca824d6d4c175c1974f
camylla-araujo <camylla.a_@hotmail.com>
2022-10-15 10:51:40 -0300

    Atualizando carousel

--- e-commerce/src/components/Carousel/index.js
+++ e-commerce/src/components/Carousel/index.js
@@ -1,43 +1,164 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import bg from './icons/bg.svg';
 import circle from './icons/circle.svg';
-import nike from  './icons/nike.svg';
+import nike from './icons/nike.svg';
 
 import './style.css';
 
-export default function Carousel() {
-  return(
+export default function CarouselImagem() {
+  return (
+    <Carousel className="carousel-principal">
+      <Carousel.Item>
+        <div style={{ backgroundImage: `url(${bg})` }} className="linha">
 
-    <div className="carouselprincipal">
-
-      <div>
-        <div style={{ backgroundImage: {bg} }} className="linha">
-
-          <img className="nike" src={nike} alt="nike" />
           <img className="circle" src={circle} alt="circle" />
+          <img className="nike" src={nike} alt="nike" />
         </div>
-        <div className="esquerda">
-          <div className="wrapper-texto1">
-            <p className="texto1"> Melhores ofertas personalizadas</p>
-          </div>
 
-          <div className="wrapper-texto2">
-            <h2 className='texto2'>
-              Queima de <br />stoque Nike 🔥
-            </h2>
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div className="wrapper-texto1">
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div className="wrapper-texto2">
+              <h2 className='texto2'>
+                Queima de <br />estoque Nike 🔥
+              </h2>
+            </div>
+            <div className="wrapper-texto3">
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
           </div>
-          <div className="wrapper-texto3">
-            <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div style={{ backgroundImage: `url(${bg})` }} className="linha">
+
+          <img className="circle" src={circle} alt="circle" />
+          <img className="nike" src={nike} alt="nike" />
+        </div>
+
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div className="wrapper-texto1">
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div className="wrapper-texto2">
+              <h2 className='texto2'>
+                Queima de <br />estoque Nike 🔥
+              </h2>
+            </div>
+            <div className="wrapper-texto3">
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
           </div>
-          <div className="wrapper-button1">
-            <a href="">
-              <button className="buttonofertas">Ver Ofertas</button>
-            </a>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div style={{ backgroundImage: `url(${bg})` }} className="linha">
+
+          <img className="circle" src={circle} alt="circle" />
+          <img className="nike" src={nike} alt="nike" />
+        </div>
+
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div className="wrapper-texto1">
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div className="wrapper-texto2">
+              <h2 className='texto2'>
+                Queima de <br />estoque Nike 🔥
+              </h2>
+            </div>
+            <div className="wrapper-texto3">
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
           </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div style={{ backgroundImage: `url(${bg})` }} className="linha">
+
+          <img className="circle" src={circle} alt="circle" />
+          <img className="nike" src={nike} alt="nike" />
+        </div>
+
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div className="wrapper-texto1">
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div className="wrapper-texto2">
+              <h2 className='texto2'>
+                Queima de <br />estoque Nike 🔥
+              </h2>
+            </div>
+            <div className="wrapper-texto3">
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
           </div>
-      </div>
-      
-      
-      </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div style={{ backgroundImage: `url(${bg})` }} className="linha">
+
+          <img className="circle" src={circle} alt="circle" />
+          <img className="nike" src={nike} alt="nike" />
+        </div>
+
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div className="wrapper-texto1">
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div className="wrapper-texto2">
+              <h2 className='texto2'>
+                Queima de <br />estoque Nike 🔥
+              </h2>
+            </div>
+            <div className="wrapper-texto3">
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+    </Carousel>
   );
 }
