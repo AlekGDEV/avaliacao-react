0fea7e7b3bdbc1281df37592b9b6d148ac13110a
TalysonSoares <talysonsoares40@gmail.com>
2022-10-16 16:52:47 -0300

    estilizando formulario de finalizar compra

--- e-commerce/src/Pages/FiinalizarCrompa/index.js
+++ e-commerce/src/Pages/FiinalizarCrompa/index.js
@@ -1,10 +1,11 @@
 import Form from "../../components/Formulario"
+import "./styles.css"
 
 
 export default function Finalizar() {
     return (
         <main>
-            <h1>Finalizar Compra</h1>
+            <h1 className="fc">Finalizar Compra</h1>
             <section>
                 <Form />
             </section>
--- e-commerce/src/components/Formulario/index.js
+++ e-commerce/src/components/Formulario/index.js
@@ -1,37 +1,38 @@
+import "./styles.css"
+
 export default function Form() {
     return (
         <div>
-            <form>
-                <h1>Informações Pessoais </h1>
-                <hr />
+            <form className="infPessoais">
+                <h1 className="ftitulo">Informações Pessoais <hr className="fhr"/> </h1>
                 
-                <label for="nome">Nome Completo *</label>
-                <input id="nome" type="text" placeholder="Insira seu nome"/>
+                <label for="nome" className="finfo">Nome Completo *</label>
+                <input id="nome" type="text" placeholder="Insira seu nome" className="finput"/>
                 
-                <label for="cpf">CPF *</label>
-                <input id="cpf" type="text" placeholder="Insira seu CPF"/>
+                <label for="cpf" className="finfo">CPF *</label>
+                <input id="cpf" type="text" placeholder="Insira seu CPF" className="finput"/>
 
-                <label for="email">E-mail *</label>
-                <input id="email" type="email" placeholder="Insira seu email" />
+                <label for="email" className="finfo">E-mail *</label>
+                <input id="email" type="email" placeholder="Insira seu email" className="finput" />
 
-                <label for="numero">Celular *</label>
-                <input id="numero" type="text" placeholder="Insira seu celular"/>
+                <label for="numero" className="finfo">Celular *</label>
+                <input id="numero" type="text" placeholder="Insira seu celular" className="finput"/>
             </form>
-            <form>
-                <h1>Informações de Entrega</h1>
-                <hr />
+
+            <form className="infEntrega">
+                <h1 className="ftitulo">Informações de Entrega <hr className="fhr"/></h1>
                 
-                <label for="endereco">Endereço *</label>
-                <input id="endereco" type="text" placeholder="Insira seu endereço" />
+                <label for="endereco" className="finfo">Endereço *</label>
+                <input id="endereco" type="text" placeholder="Insira seu endereço" className="finput" />
 
-                <label for="bairro">Bairro *</label>
-                <input id="bairro" type="text" placeholder="Insira seu bairro"/>
+                <label for="bairro" className="finfo">Bairro *</label>
+                <input id="bairro" type="text" placeholder="Insira seu bairro" className="finput"/>
 
-                <label for="cep">CEP *</label>
-                <input id="cep" type="text" placeholder="Insira seu cep" />
+                <label for="cep" className="finfo">CEP *</label>
+                <input id="cep" type="text" placeholder="Insira seu cep" className="finput" />
 
-                <label for="complemento">Complemento </label>
-                <input id="complemento" type="text" placeholder="Insira seu complemento" />
+                <label for="complemento" className="finfo">Complemento </label>
+                <input id="complemento" type="text" placeholder="Insira seu complemento" className="finput"/>
             </form>
         </div>
     )
