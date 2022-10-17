0e37e6faa5a6ce4f1e283adee8a4a6bae0aa9a76
AlekGDEV <AlexandreGDEV@outlook.com>
2022-10-16 23:14:45 -0300

    adicionando a pagina de meus pedidos

--- e-commerce/src/components/main/Article/Card/card.js
+++ e-commerce/src/components/main/Article/Card/card.js
@@ -5,16 +5,16 @@ import "./index.css";
 export default function Card(props) {
   return (
       <div class="container1">
-          <div class="imagem">
+          <div className="imagem">
           <img className="imagem" src={props.imgprod} alt="produto" />  
            </div>
          
          <div className="container2">
-             <div class="titulo-card">
+             <div className="titulo-card">
              <BotaoDesc desconto="30% OFF" />
              <h3 className="h3Title"> {props.nome} </h3>
              </div>
-             <div class="botao-de-compra">
+             <div className="botao-de-compra">
              <Comprar />
               </div>
          </div>  
