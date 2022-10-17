0d4dcecbe4bc8131318cc7f225c131b4ae74701b
ErikaNunex <erikanunes0512@gmail.com>
2022-10-17 03:00:15 -0300

    estilizando pagina meu carrinho

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -5,6 +5,7 @@ import Produtos from "./Pages/Produtos"
 import Categorias from "./Pages/Categorias"
 import MeusPedidos from "./Pages/MeusPedidos"
 import MeuCarrinho from "./Pages/MeuCarrinho/index.js";
+import VerMeuCarrinho from "./Pages/VerMeuCarrinho/index.js";
 
 export default function App() {
   return (
@@ -16,7 +17,8 @@ export default function App() {
               <Route path="/produtos" element={<Produtos/>}></Route>
               <Route path="/categorias" element={<Categorias/>}></Route>
               <Route path="/meus-pedidos" element={<MeusPedidos/>}></Route>
-              {/* <Route path="/meu-carrinho" element={<MeuCarrinho/>}></Route> */} {/* criar uma rota para "ver carrinho*/}
+              <Route path="/meu-carrinho" element={<VerMeuCarrinho/>}></Route> 
+              {/* <Route path="/finalizarCompra" element= ></Route> */}
             </Routes>
        </BrowserRouter>
 
